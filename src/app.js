6218bc766d3ac230124c2c7c62b90e71e3069baa
evil <evil@evil.evil>
2024-06-15 12:20:02 +0500

    fix: binding port

--- src/app.js
+++ src/app.js
@@ -19,3 +19,7 @@ app.use((req, res, next) => {
 initializeDatabase();
 
 app.use("/uploads", express.static(path.resolve(__dirname, "../uploads")));
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
